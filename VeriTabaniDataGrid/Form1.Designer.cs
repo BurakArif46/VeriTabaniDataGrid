7e4f7e7055a1fbf730c6ae43e45ce928058a4d7d
burak753 <burak753@RPC01>
2024-05-14 08:50:43 +0300

    Proje dosyası ekle.

---
+++ VeriTabaniDataGrid/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace VeriTabaniDataGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.kutuphane753DataSet = new VeriTabaniDataGrid.Kutuphane753DataSet();
+            this.tableTableAdapter = new VeriTabaniDataGrid.Kutuphane753DataSetTableAdapters.TableTableAdapter();
+            this.ıDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kitapAdiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kitapTuruDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kitapYazariDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kutuphane753DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ıDDataGridViewTextBoxColumn,
+            this.kitapAdiDataGridViewTextBoxColumn,
+            this.kitapTuruDataGridViewTextBoxColumn,
+            this.kitapYazariDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(224, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(453, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.kutuphane753DataSet;
+            // 
+            // kutuphane753DataSet
+            // 
+            this.kutuphane753DataSet.DataSetName = "Kutuphane753DataSet";
+            this.kutuphane753DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // ıDDataGridViewTextBoxColumn
+            // 
+            this.ıDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.ıDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.ıDDataGridViewTextBoxColumn.Name = "ıDDataGridViewTextBoxColumn";
+            // 
+            // kitapAdiDataGridViewTextBoxColumn
+            // 
+            this.kitapAdiDataGridViewTextBoxColumn.HeaderText = "Kitap Adı";
+            this.kitapAdiDataGridViewTextBoxColumn.Name = "kitapAdiDataGridViewTextBoxColumn";
+            // 
+            // kitapTuruDataGridViewTextBoxColumn
+            // 
+            this.kitapTuruDataGridViewTextBoxColumn.DataPropertyName = "KitapTuru";
+            this.kitapTuruDataGridViewTextBoxColumn.HeaderText = "Kitap Türü";
+            this.kitapTuruDataGridViewTextBoxColumn.Name = "kitapTuruDataGridViewTextBoxColumn";
+            // 
+            // kitapYazariDataGridViewTextBoxColumn
+            // 
+            this.kitapYazariDataGridViewTextBoxColumn.DataPropertyName = "KitapYazari";
+            this.kitapYazariDataGridViewTextBoxColumn.HeaderText = "Kitap Yazarı";
+            this.kitapYazariDataGridViewTextBoxColumn.Name = "kitapYazariDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 380);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kutuphane753DataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Kutuphane753DataSet kutuphane753DataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private Kutuphane753DataSetTableAdapters.TableTableAdapter tableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ıDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kitapAdiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kitapTuruDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kitapYazariDataGridViewTextBoxColumn;
+    }
+}
+
